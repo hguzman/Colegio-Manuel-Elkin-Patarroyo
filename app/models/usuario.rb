d7a2f5df2bbed77af208bfa93371976248835823
mateo <balzamateo5@gmail.com>
2020-08-20 18:00:37 -0500

    Se modificarón estilos de la landing page

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,7 +3,7 @@ class Usuario < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :registerable
 
   has_many :anotaciones
   has_many :materias
