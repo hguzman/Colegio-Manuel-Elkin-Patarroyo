3dc09f8516655bfbd42b078ffb4ed9e9153e538b
mateo <balzamateo5@gmail.com>
2020-12-08 10:02:38 -0500

    Prueba de despligue Inicio de sesion7

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ApplicationRecord
     UserMailer.bienvenida_mailer(@user).deliver_now
   end
 
-  validates :identificacion, :primer_nombre, :primer_apellido,:segundo_apellido, :telefono, :direccion, presence: true
+  validates :identificacion, :primer_nombre, :primer_apellido,:segundo_apellido, :direccion, presence: true
   validates :telefono,numericality: { only_integer: true }, length: { maximum: 10 }
   validates :identificacion, :email, uniqueness: true
   validates :identificacion, length: { maximum: 10 }
