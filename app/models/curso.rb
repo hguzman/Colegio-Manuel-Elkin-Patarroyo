813690f522a987fbcf0c1e4708c4e2c3591d24e0
mateo <balzamateo5@gmail.com>
2020-12-09 08:50:38 -0500

    Se modifico el envio de correo

--- app/models/curso.rb
+++ app/models/curso.rb
@@ -2,7 +2,7 @@ class Curso < ApplicationRecord
     resourcify
     has_many :users, dependent: :nullify
     accepts_nested_attributes_for :users
-    has_many :materias, dependent: :destroy  
+    has_many :materias, dependent: :nullify  
     
     validates :codigo, :nombre, presence: true
     validates :codigo, uniqueness: true, numericality: { greater_than: 0, less_than: 99 }
